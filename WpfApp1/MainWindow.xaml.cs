320ce80b06c65a9f9cf02a3c9afb4bf7d6a41235
anirams <marina.stvoric1@gmail.com>
2019-04-01 10:10:33 +0200

    all

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using WpfApp1.PersonClasses;
 
 namespace WpfApp1
 {
@@ -23,6 +25,26 @@ namespace WpfApp1
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void InsertButton_Click(object sender, RoutedEventArgs e)
+        {
+            PersonClass person = new PersonClass();
+            person.FirstName = firstNameTextBox.Text;
+            person.LastName = lastNameTextBox.Text;
+            person.Age = Int32.Parse(ageTextBox.Text);
+            person.Insert(person);
+            firstNameTextBox.Text = "";
+            lastNameTextBox.Text = "";
+            ageTextBox.Text = "";
+        }
+
+        private void RefreshButton_Click(object sender, RoutedEventArgs e)
+        {
+            DataTable dt = new DataTable();
+            PersonClass person = new PersonClass();
+            dt = person.Return();
+            podaciDataGrid.DataContext = dt.DefaultView;
         }
     }
 }
---
+++ WpfApp1/PersonClasses/PersonClass.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Data;
+
+namespace WpfApp1.PersonClasses
+{
+    class PersonClass
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+
+        string connstring = ConfigurationManager.ConnectionStrings["connstring"].ConnectionString;
+
+        public bool Insert(PersonClass person)
+        {
+            bool issuccess = false;
+
+            SqlConnection conn = new SqlConnection(connstring);
+            SqlCommand cmd = new SqlCommand("INSERT INTO Person(FirstName, LastName, Age) VALUES (@FirstName, @LastName, @Age)", conn);
+            cmd.Parameters.AddWithValue("@FirstName", person.FirstName);
+            cmd.Parameters.AddWithValue("@LastName", person.LastName);
+            cmd.Parameters.AddWithValue("@Age", person.Age);
+            conn.Open();
+            int rows = cmd.ExecuteNonQuery();
+            conn.Close();
+            if (rows > 0)
+            {
+                issuccess = true;
+                return issuccess;
+            }
+            return issuccess;
+        }
+
+        public DataTable Return()
+        {
+            DataTable dt = new DataTable();
+            SqlConnection conn = new SqlConnection(connstring);
+            SqlCommand cmd = new SqlCommand("SELECT * FROM Person", conn);
+            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+            conn.Open();
+            adapter.Fill(dt);
+            conn.Close();
+            return dt;
+        }
+    }
+}
